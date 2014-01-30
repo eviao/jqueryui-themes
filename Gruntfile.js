7f9ceade488afec39934eb3c66b871670f061e51
Eviao <eviao.dev@gmail.com>
2014-01-30 14:37:34 +0800

    添加default主题

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,28 @@ module.exports = function(grunt) {
           "themes/base/css/jquery.ui.base.css": "themes/base/less/jquery.ui.base.less",
           "themes/base/css/jquery.ui.all.css": "themes/base/less/jquery.ui.all.less",
         }
+      },
+      default: {
+        files: {
+          "themes/default/css/jquery.ui.accordion.css": "themes/default/less/jquery.ui.accordion.less",
+          "themes/default/css/jquery.ui.autocomplete.css": "themes/default/less/jquery.ui.autocomplete.less",
+          "themes/default/css/jquery.ui.button.css": "themes/default/less/jquery.ui.button.less",
+          "themes/default/css/jquery.ui.core.css": "themes/default/less/jquery.ui.core.less",
+          "themes/default/css/jquery.ui.datepicker.css": "themes/default/less/jquery.ui.datepicker.less",
+          "themes/default/css/jquery.ui.dialog.css": "themes/default/less/jquery.ui.dialog.less",
+          "themes/default/css/jquery.ui.menu.css": "themes/default/less/jquery.ui.menu.less",
+          "themes/default/css/jquery.ui.progressbar.css": "themes/default/less/jquery.ui.progressbar.less",
+          "themes/default/css/jquery.ui.resizable.css": "themes/default/less/jquery.ui.resizable.less",
+          "themes/default/css/jquery.ui.selectable.css": "themes/default/less/jquery.ui.selectable.less",
+          "themes/default/css/jquery.ui.slider.css": "themes/default/less/jquery.ui.slider.less",
+          "themes/default/css/jquery.ui.spinner.css": "themes/default/less/jquery.ui.spinner.less",
+          "themes/default/css/jquery.ui.tabs.css": "themes/default/less/jquery.ui.tabs.less",
+          "themes/default/css/jquery.ui.tooltip.css": "themes/default/less/jquery.ui.tooltip.less",
+          "themes/default/css/jquery.ui.theme.css": "themes/default/less/jquery.ui.theme.less",
+
+          "themes/default/css/jquery.ui.base.css": "themes/default/less/jquery.ui.base.less",
+          "themes/default/css/jquery.ui.all.css": "themes/default/less/jquery.ui.all.less",
+        }
       }
     },
     
